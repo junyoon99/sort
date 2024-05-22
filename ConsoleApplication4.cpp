89b5547e5446628f61933da2c76f8c7249f88554
Hyeontae An <83248120+bearbig-12@users.noreply.github.com>
2024-05-22 17:53:49 +0900

    0522

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -1,65 +1,77 @@
 ﻿#include <iostream>
-#include <chrono>
 
 using Comparison = bool(*)(int, int);
 
 bool Ascending(int x, int y) 
 {
-    return x > y;
+	return x > y;
 }
 
 bool Descending(int x, int y) 
 {
-    return x < y;
+	return x < y;
 }
 
 
-void Sort(int numbers[], int count, Comparison f) 
+void sequentialSort(int numbers[], int count, Comparison f) 
 {
-    for (int i = 0; i < count; i++) 
-    {
-        for (int j = 0; j < count; j++) 
-        {
-            if (f(numbers[i], numbers[j])) 
-            {
-                int temp = numbers[i];
-                numbers[i] = numbers[j];
-                numbers[j] = temp;
-            }
-        }
-    }
+	for (int i = 0; i < count-1; i++) 
+	{
+		for (int j = i+1; j < count; j++) 
+		{
+			if (f(numbers[i], numbers[j])) 
+			{
+				int temp = numbers[i];
+				numbers[i] = numbers[j];
+				numbers[j] = temp;
+			}
+		}
+	}
 }
+
+for (int i = 0; i < count; i++) 
+{
+	for (int j = i+1; j < count; j++) 
+	{
+		if (numbers[i] > numbers[j]) 
+		{
+			i = j;
+			break;
+		}
+	}
+}
+
 
 int add(int x, int y) 
 {
-    return x + y;
+	return x + y;
 }
 
 int Sum(int numbers[], int count) 
 {
-    int total{};
-    for (int i = 0; i < count; i++) 
-    {
-        total += numbers[i];
-    }
-    return total;
+	int total{};
+	for (int i = 0; i < count; i++) 
+	{
+		total += numbers[i];
+	}
+	return total;
+}
+
+void Printarray(int input[], int count) 
+{
+	for (int i = 0; i < count; i++)
+	{
+		std::cout << input[i] << " ";
+	}
+	std::cout << std::endl;
 }
 
 int main()
 {
-    const int SIZE_ARRAY{ 10 };
-    int scores[SIZE_ARRAY]{ 20,10,40,30,70,90,80,60,50,100 };
+	const int SIZE_ARRAY{ 10 };
+	int scores[SIZE_ARRAY]{ 20,10,40,30,70,90,80,60,50,100 };
 
-    auto startTime = std::chrono::system_clock::now();
-    Sort(scores, SIZE_ARRAY, Ascending);
-    auto endTime = std::chrono::system_clock::now();
+	sequentialSort(scores, SIZE_ARRAY, Ascending);
 
-    auto duration = endTime - startTime;
-
-    std::cout << "Sort() - " << duration.count() << "ms" << std::endl;
-
-    for (int i = 0; i < SIZE_ARRAY; i++) 
-    {
-        std::cout << scores[i] << std::endl;
-    }
+	Printarray(scores, SIZE_ARRAY);
 }
